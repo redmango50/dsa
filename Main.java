fadba5fa8cc20abf382adde75194d79085597515
Redmango50 <redmango5050@gmail.com>
2025-10-23 13:44:18 +0600

    added selection sort

--- Main.java
+++ Main.java
@@ -136,11 +136,12 @@ public class Main{
         return os.contains("win");
     }
     public static void sort(Scanner input, int[] array){
-        String[] options = {"1. Bubble sort"};
+        String[] options = {"1. Bubble sort", "2. Selection sort"};
         char symbol = '>';
         searchOptions(options);
         switch (userCommand(input, symbol)) {
             case "bubble", "1" -> bubbleSort(array);
+            case "selection", "2" -> selectionSort(array);
             case "back", "0" -> System.out.println("Returning...");
             default -> System.out.println("Invalid option!");
         }
@@ -158,5 +159,23 @@ public class Main{
         }
         System.out.println("Array sorted! (bubble sort)");
         System.out.println("New array: " +Arrays.toString(array));
+    }
+    public static void selectionSort(int[] array) {
+        System.out.println("Array: " + Arrays.toString(array));
+
+        for (int i = 0; i < array.length - 1; i++) {
+            int maxIndex = 0;
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j] > array[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            int temp = array[array.length - i - 1];
+            array[array.length - i - 1] = array[maxIndex];
+            array[maxIndex] = temp;
+        }
+
+        System.out.println("Array sorted! (selection sort)");
+        System.out.println("New array: " + Arrays.toString(array));
     }
 }
